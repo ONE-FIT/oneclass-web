fc1d4830133dba992f1e9c6f40e02782233644f8
chaeyn <chaulfe@gmail.com>
2025-09-16 17:56:45 +0900

    feat: 로그인 페이지 뷰

---
+++ apps/teacher/src/pages/Signin.tsx
@@ -0,0 +1,68 @@
+import OneClassAppImg from "@/assets/oneclass-app.png?url";
+import OneclassLogo from "@/assets/one-class-logo.svg?url";
+
+function Signin() {
+  return (
+    <div className="grid h-screen grid-cols-7">
+      <div className="flex flex-col gap-20 bg-white col-span-3 p-24 justify-center mb-20">
+        <div className="flex flex-col gap-6">
+          <div className="flex gap-2 items-end">
+            <img className="w-12" src={OneclassLogo} alt="Oneclass Logo" />
+            <p className="text-2xl font-normal">OneClass</p>
+          </div>
+          <h2 className="text-5xl font-bold">Welcome!</h2>
+        </div>
+        <form className="flex flex-col gap-20">
+          <div className="flex flex-col gap-2">
+            <input
+              type="text"
+              placeholder="아이디"
+              className="border border-gray-300 p-3 px-4 rounded"
+            />
+            <input
+              type="current-password"
+              placeholder="비밀번호"
+              className="border border-gray-300 p-3 px-4 rounded"
+            />
+            <div className="flex items-center gap-2 ml-0.5">
+              <label htmlFor="remember" className="font-semibold hover:cursor-pointer">
+                로그인 유지
+              </label>
+              <input
+                className="hover:cursor-pointer"
+                type="checkbox"
+                name="remember"
+                id="remember"
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="bg-[#001222] text-white py-4 rounded-4xl w-full hover:cursor-pointer"
+          >
+            로그인
+          </button>
+        </form>
+      </div>
+      <div className="bg-[#D8EAFC] col-span-4 h-screen">
+        <div className="flex flex-col gap-8 px-24 justify-center">
+          <h1 className="text-7xl font-bold mt-68">학원관리</h1>
+          <div className="flex flex-col gap-4">
+            <h3 className="text-2xl font-semibold">
+              OneClass는 학생과 학원을 이어주는 <br />
+              온라인 학원 관리 플랫폼입니다
+            </h3>
+            <button className="border-2 w-36 py-2 rounded hover:cursor-pointer">문의하기</button>
+          </div>
+        </div>
+        <img
+          className="w-140 relative z-100 left-60 top-20"
+          src={OneClassAppImg}
+          alt="oneclass app img"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Signin;
--- apps/teacher/src/router.tsx
+++ apps/teacher/src/router.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Signin from "./pages/Signin";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="signin" element={<Signin />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
